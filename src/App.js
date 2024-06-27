bf8eacaeda8d5e06ddd89a0f9948004ed4fc6085
KumaresanTechjays <76718563+KumaresanIderize@users.noreply.github.com>
2024-06-27 15:01:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello amplify
+          Hello Techjays
         </a>
       </header>
     </div>
